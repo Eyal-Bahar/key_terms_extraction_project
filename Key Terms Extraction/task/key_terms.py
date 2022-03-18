fc512fc8939f4977f6c7d1172286dc37559bbc22
Eyal_Bahar <eyal.bhr@gmail.com>
2022-03-18 21:08:01 +0700

    stage 3 complete

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -8,23 +8,53 @@ from nltk.corpus import stopwords
 import string
 import nltk
 # nltk.download('wordnet')
+# nltk.download('averaged_perceptron_tagger')
 
-xml_file = r"C:\hyper_skill\Key Terms Extraction\Key Terms Extraction\task\news.xml"
-root = etree.parse(xml_file).getroot()
-# etree.dump(root)
-tokens = []
-lemmatizer = WordNetLemmatizer()
-for corpus in root[0]:
-    for value in corpus:
-        if value.get('name') == "head":
-            print(value.text + ":")
-        if value.get('name') == "text":
-            raw_token = word_tokenize(value.text.lower())
-            lemma_token = [lemmatizer.lemmatize(inst) for inst in raw_token]
-            filter_list = stopwords.words('english')
-            filter_list += list(string.punctuation)
-            token = [inst for inst in lemma_token if inst not in filter_list]
-            token = sorted(token, reverse=True)
-            wat = Counter(token).most_common(5)
-            most_common = [key for key, value in wat]
-    print(*most_common)
+
+def get_rood_from_xml(xml_path=None):
+    if xml_path is None:
+        xml_path = r"C:\hyper_skill\Key Terms Extraction\Key Terms Extraction\task\news.xml"
+    return etree.parse(xml_path).getroot()
+
+
+def get_most_common(token):
+    token = sorted(token, reverse=True)
+    wat = Counter(token).most_common(5)
+    most_common = [key for key, value in wat]
+    return most_common
+
+
+def filter_token(token):
+    filter_list = stopwords.words('english')
+    filter_list += list(string.punctuation)
+    filtered_token = [inst for inst in token if inst not in filter_list]
+    return filtered_token
+
+
+def extract_nouns(filtered_tokens):
+    nouned_tokens = []
+    for token in filtered_tokens:
+        tagged = nltk.pos_tag([token])
+        if tagged[0][1] == "NN":
+            nouned_tokens.append(tagged[0][0])
+    return nouned_tokens
+
+def main():
+
+    root = get_rood_from_xml()
+
+    lemmatizer = WordNetLemmatizer()
+    for corpus in root[0]:
+        for value in corpus:
+            if value.get('name') == "head":
+                print(value.text + ":")
+            if value.get('name') == "text":
+                raw_tokens = word_tokenize(value.text.lower())
+                lemmatized_tokens = [lemmatizer.lemmatize(raw_token) for raw_token in raw_tokens]
+                filtered_tokens = filter_token(lemmatized_tokens)
+                noun_tokens = extract_nouns(filtered_tokens)
+                most_common = get_most_common(noun_tokens)
+        print(*most_common)
+
+if __name__ == '__main__':
+    main()
